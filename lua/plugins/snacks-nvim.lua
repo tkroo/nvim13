5c1bd5273334c5205f2fd2cc4d351301ba856781
David Sullivan <treatkor@gmail.com>
2026-05-05 22:53:50 -0700

    change snacks dashboard

--- lua/plugins/snacks-nvim.lua
+++ lua/plugins/snacks-nvim.lua
@@ -8,7 +8,22 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+      preset = {
+        header=[[
+╭╮╷╭─╴╭─╮╷ ╷╷╭┬╮
+│╰┤├╴ │ ││╭╯││││
+╵ ╵╰─╴╰─╯╰╯ ╵╵ ╵]],
+        enabled = true
+      },
+    },
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
