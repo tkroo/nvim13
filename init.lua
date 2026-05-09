14428b76644ed50978d71645b4e826682dc3aac8
David Sullivan <treatkor@gmail.com>
2026-05-09 13:37:58 -0700

    dunno

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
+require('vim._core.ui2').enable({
+  enable = true,
+  msg = {
+    target = "cmd",
+    pager = { height = 0.5 },
+    dialog ={ height = 0.5 },
+    cmd = { height = 0.5 },
+    msg ={ height = 0.5, timeout = 4500 },
+  }
+})
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
